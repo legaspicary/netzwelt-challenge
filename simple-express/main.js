341d05e5a5512f0804acc04b63bf98722006091f
legaspicary <legaspicary@gmail.com>
2023-02-16 01:20:31 +0800

    set port as an env var

--- simple-express/main.js
+++ simple-express/main.js
@@ -3,7 +3,7 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 const app = express();
-const port = 3001;
+const port = process.env.API_PORT ?? 3000;
 
 dotenv.config({ path: "../.env" });
 
